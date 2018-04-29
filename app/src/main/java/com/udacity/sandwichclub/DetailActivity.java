0a113b5a91deca430ed26aea3043f160e8d20055
Diaz Agasatya <diazagasatya@gmail.com>
2018-04-28 21:24:39 -0700

    PopulateUI() and DetailLayout Final Layout

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,13 +16,21 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    // Access sandwich details
+    private TextView mAlsoKnownAs, mPlaceOfOrigin, mIngredients, mDescription;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        // Bind the text-views and image-view with the layout
         ImageView ingredientsIv = findViewById(R.id.image_iv);
-
+        mAlsoKnownAs = findViewById(R.id.also_known_tv);
+        mPlaceOfOrigin = findViewById(R.id.origin_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
+        mDescription = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -44,7 +53,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -64,7 +73,48 @@ public class DetailActivity extends AppCompatActivity {
      * In here you will set the textView and imageView to the corresponding data grabbed
      * from JsonUtils.
      */
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        // Represent the list<Strings> as single string separated by commas
+        StringBuilder alsoKnownAsString = new StringBuilder();
+        int alsoKnownAsLength = sandwich.getAlsoKnownAs().size();
+        StringBuilder ingredientsString = new StringBuilder();
+        int ingredientsLength = sandwich.getIngredients().size();
+
+        // Grab all elements in alsoKnownAs and ingredients
+        if(alsoKnownAsLength > 1) {
+            for(int i = 0; i < alsoKnownAsLength; i++) {
+                alsoKnownAsString.append(sandwich.getAlsoKnownAs().get(i));
+
+                // if reached end element don't add comma
+                if(i < alsoKnownAsLength - 1) {
+                    alsoKnownAsString.append(", ");
+                }
+            }
+        }
+        else {
+            // Not available
+            alsoKnownAsString = new StringBuilder("N/A");
+        }
+
+        if(ingredientsLength > 1) {
+            for(int i = 0; i < ingredientsLength; i++) {
+                ingredientsString.append(sandwich.getIngredients().get(i));
+
+                // if reached end element don't add comma
+                if(i < ingredientsLength - 1) {
+                    ingredientsString.append(", ");
+                }
+            }
+        }
+        else {
+            ingredientsString = new StringBuilder("N/A");
+        }
+
+        // Set textViews here
+        mAlsoKnownAs.setText(alsoKnownAsString.toString());
+        mDescription.setText(sandwich.getDescription());
+        mIngredients.setText(ingredientsString.toString());
+        mPlaceOfOrigin.setText(sandwich.getPlaceOfOrigin());
     }
 }
